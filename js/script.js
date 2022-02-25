acbf2ed6563ea99ee0285f939c9064fe36330826
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-02-25 16:33:40 +0100

    added comments

--- js/script.js
+++ js/script.js
@@ -3,24 +3,30 @@
 // controlla che sia nella lista di chi può accedere, (creo un array con la lista delle mail, controllo scorrendo con un ciclo for l'array che l'input corrrisponda)
 // stampa un messaggio appropriato sull’esito del controllo. (creo elemento html con risposta positivo/negativa)
 
+
+// created array with the list of the email for comparison
 const listEmail = ["boolean@boolean.it", "alessandro@boolean.it", "richard@boolean.it", "tom@boolean.it", "alexis@boolean.it", "denzel@boolean.it", "cristiano@boolean.it"];
 
+// variable for add dom element (output)
 let newBox = document.createElement("div");
 
+// event that listens to the click of the button by the user
 document.querySelector("button").addEventListener("click", function () {
 
-    
+    // variable that counts if user input is present in the array
+    let confirmVerification = 0;
 
-    let confermVerification = 0;
-
+    // variable that takes the data entered by the user
     const userEmail = document.getElementById("input-email").value;
 
     
-
+    // for loop that flows the array through the i(index)
     for(let i = 0; i < listEmail.length; i++){
+
+        // condition that checks if there is correspondence between the user data and each element of the array
         if(listEmail[i] == userEmail){
             
-            confermVerification++;
+            confirmVerification++;
             
         }
         else{
@@ -28,7 +34,7 @@ document.querySelector("button").addEventListener("click", function () {
         }
     }
 
-    if (confermVerification > 0) {
+    if (confirmVerification > 0) {
         
         newBox.classList.remove("border-success");
         newBox.classList.add("border", "border-3", "border-danger", "mt-3", "p-3");
