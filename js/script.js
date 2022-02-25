80152da326717a66c4350a9742d93fcbcb0529be
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-02-25 15:15:08 +0100

    realized program js dice game

--- js/script.js
+++ js/script.js
@@ -30,3 +30,29 @@ document.querySelector("button").addEventListener("click", function () {
     }
     
 });
+
+
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.(genero due numeri random)
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.(li confronto)
+
+
+document.getElementById("play-game").addEventListener("click", function () {
+
+    const userNumber = Math.floor((Math.random()* 6) + 1);
+    console.log(userNumber);
+
+    const pcNumber = Math.floor((Math.random()* 6) + 1);
+    console.log(pcNumber);
+
+    if (userNumber > pcNumber) {
+        console.log("hai vinto");
+        
+    } else if(userNumber < pcNumber){
+        console.log("hai perso");
+    }else{
+        console.log("pareggio, ritenta");
+    }
+    
+})
